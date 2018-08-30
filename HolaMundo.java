33b5b0b2cc5ed7778430913926b56f5379f4ab2c
Jose Cornejo <jose.cornejo.mella@gmail.com>
2018-08-30 14:22:52 -0300

    comprimir

---
+++ HolaMundo.java
@@ -0,0 +1,12 @@
+public class HolaMundo
+{
+	
+	public static void main (String []arg)
+	{
+		
+		System.out.println("hola");
+		System.out.println("hola2");
+		System.out.println("hola3");
+	}
+	
+}
